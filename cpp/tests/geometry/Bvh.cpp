390fba49dbca226f6104a1795451f47a2881f29c
Matt Jarvis <mattj23@gmail.com>
2021-03-11 01:54:13 -0500

    initial node unit tests

--- cpp/tests/geometry/Bvh.cpp
+++ cpp/tests/geometry/Bvh.cpp
@@ -25,16 +25,15 @@
 // ----------------------------------------------------------------------------
 
 #include "open3d/geometry/Bvh.h"
+
 #include "tests/UnitTest.h"
 
 using namespace open3d::geometry;
 using namespace Eigen;
 using namespace ::testing;
 
-namespace open3d {
-namespace tests {
-
-namespace bvh_test {
+using AABB = AxisAlignedBoundingBox;
+using BoxVec = std::vector<AABB>;
 
 /// Simple primitive for testing the BVH and its components
 struct Sphere {
@@ -46,15 +45,70 @@ struct Sphere {
         return {center - r, center + r};
     }
 };
+
+std::unique_ptr<bvh::BvhNode<Sphere>> make_node_box(Vector3d min,
+                                                    Vector3d max) {
+    BoxVec vec{{min, max}};
+    auto node = std::make_unique<bvh::BvhNode<Sphere>>();
+    node->indices_.push_back(0);
+    node->SetBox(vec);
+    return node;
 }
 
-TEST(BvhNode, NodeCreate) {
-    using namespace bvh_test;
-    auto node = bvh::BvhNode<Sphere>();
+namespace open3d {
+namespace tests {
+
+TEST(BvhNode, EmptyNodeCreate) {
+    bvh::BvhNode<Sphere> node;
 
     EXPECT_TRUE(node.indices_.empty());
     EXPECT_TRUE(node.IsLeaf());
+    EXPECT_TRUE(node.Box().IsEmpty());
 }
 
+TEST(BvhNode, IsLeaf) {
+    bvh::BvhNode<Sphere> node;
+    node.left_ = std::make_unique<bvh::BvhNode<Sphere>>();
+    EXPECT_FALSE(node.IsLeaf());
 }
+
+TEST(BvhNode, SetBoxLeaf) {
+    BoxVec vec{{{-10, -1, -1}, {10, 1, 1}}, {{-1, -7, -1}, {1, 7, 1}}};
+    bvh::BvhNode<Sphere> node;
+    node.indices_.push_back(0);
+    node.indices_.push_back(1);
+    node.SetBox(vec);
+
+    ExpectEQ(node.Box().min_bound_, {-10, -7, -1});
+    ExpectEQ(node.Box().max_bound_, {10, 7, 1});
 }
+
+TEST(BvhNode, SetBoxNonLeaf) {
+    bvh::BvhNode<Sphere> node;
+    BoxVec v;
+    node.left_ = make_node_box({-5, -1, -1}, {5, 1, 1});
+    node.right_ = make_node_box({-1, -6, -1}, {1, 6, 1});
+    node.SetBox(v);
+
+    ExpectEQ(node.Box().min_bound_, {-5, -6, -1});
+    ExpectEQ(node.Box().max_bound_, {5, 6, 1});
+}
+
+TEST(BvhNode, SplitLeafTwoEntites) { EXPECT_TRUE(false); }
+
+TEST(BvhNode, SplitLeafMinPrimitives) { EXPECT_TRUE(false); }
+
+TEST(BvhNode, VolumeAbort) { EXPECT_TRUE(false); }
+
+TEST(BvhNode, SplitLeafCardinalY) { EXPECT_TRUE(false); }
+
+TEST(BvhNode, SplitLeafCardinalZ) { EXPECT_TRUE(false); }
+
+TEST(BvhNode, SplitLeafCardinalX) { EXPECT_TRUE(false); }
+
+TEST(Bvh, CreateTopDown) { EXPECT_TRUE(false); }
+
+TEST(Bvh, PossibleIntersections) { EXPECT_TRUE(false); }
+
+}  // namespace tests
+}  // namespace open3d
